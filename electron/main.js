fc8f40ba7b5192a487adac7553cc85e262905a77
coder3306 <coder3306@goodsoft.io>
2025-01-02 15:31:28 +0900

    feat(project): initialize with Electron and Vue CLI

---
+++ electron/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
+  });
+  mainWindow.webContents.openDevTools();
+  mainWindow.loadURL("http://localhost:8080");
+});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.devtool("source-map");
+  },
 });
