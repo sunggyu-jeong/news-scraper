07db867cc5c55196ba9fc4b91843f107c04a9e5e
coder3306 <jskoder0109@gmail.com>
2025-01-21 11:55:35 +0900

    fix(package). 불필요 패키징 제거

--- vue.config.js
+++ vue.config.js
@@ -9,24 +9,8 @@ module.exports = defineConfig({
   outputDir: "dist",
   configureWebpack: {
     resolve: {
-      fallback: {
-        util: require.resolve("util/"),
-        stream: require.resolve("stream-browserify"),
-        path: require.resolve("path-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-        os: require.resolve("os-browserify/browser"),
-        vm: require.resolve("vm-browserify"),
-        fs: false,
-        net: false,
-        child_process: false,
-      },
       extensions: ["*", ".js", ".vue", ".json"],
     },
-    plugins: [
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-        Buffer: ["buffer", "Buffer"],
-      }),
-    ],
+    plugins: [],
   },
 });
