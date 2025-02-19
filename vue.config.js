d07ed53cb9314235490a9033ae18d76b5c4326f1
coder3306 <jskoder0109@gmail.com>
2025-02-19 13:27:51 +0900

    feat. 타입스크립트 마이그레이션 진행 중

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,35 @@
 const { defineConfig } = require("@vue/cli-service");
-const webpack = require("webpack");
 
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack: (config) => {
+    // 소스 맵 설정
     config.devtool("source-map");
+
+    // TypeScript 확장자를 resolve.extensions에 추가
+    config.resolve.extensions.add(".ts").add(".tsx");
   },
   outputDir: "dist",
   configureWebpack: {
     resolve: {
-      extensions: ["*", ".js", ".vue", ".json"],
+      // .ts, .vue, .js, .json 등의 확장자에 대한 처리
+      extensions: ["*", ".ts", ".tsx", ".js", ".vue", ".json"],
     },
-    plugins: [],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: "ts-loader", // TypeScript 파일 처리
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/], // .vue 파일 안에 있는 TypeScript 코드 처리
+          },
+        },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+      ],
+    },
   },
 });
