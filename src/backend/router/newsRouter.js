8e0618e9f17c7d7cae5f1331df501aa87af818bf
coder3306 <jskoder0109@gmail.com>
2025-02-20 10:29:18 +0900

    chore. 불필요 파일 삭제

--- src/backend/router/newsRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { getNews } = require("../crawler/newsController.js");
-
-const router = Router();
-
-router.get("/news", getNews);
-
-module.exports = router;
